e7c75417541977bec151b61b19f2a0776723d3f5
Ilya <ilyafateev@borrowlabs.com>
2022-08-05 23:58:48 +0300

    init project

---
+++ src/components/Tag.vue
@@ -0,0 +1,24 @@
+<template>
+  <span
+    @click="clickOnTag($event)"
+    class="inline-flex items-center px-2 m-1 rounded-md text-xs font-medium bg-gray-300 text-gray-800 cursor-pointer"
+  >
+    {{ ticker.Symbol }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Tag",
+
+  props: {
+    ticker: null
+  },
+
+  methods: {
+    clickOnTag(event) {
+      this.$emit("clickOnTag", event.target.innerText);
+    }
+  }
+};
+</script>
